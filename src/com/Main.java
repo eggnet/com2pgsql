fb4f147e5e9b5e935639888eaba58b456d1b8024
Jordan Ell <jordan.ell7@gmail.com>
2012-06-06 10:38:28 -0700

    This commit makes the DB get created. You must pass in the first parameter as the DB name. fixes #5

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,14 +1,34 @@
 package com;
 
+import java.io.IOException;
+
 import db.ComDb;
 
 public class Main
 {
 	public static ComDb db;
-	public static void Main(String[] args)
+	public static void main(String[] args)
 	{
-		db = new ComDb();
-		db.connect("");
-		db.createDb(args[1]);
+		System.out.println("Scm2Pgsql tool developed by eggnet at UVic.");
+		try {
+			if (args.length < 1)
+			{
+				throw new ArrayIndexOutOfBoundsException();
+			}
+			else
+			{
+				// Create the DB
+				db = new ComDb();
+				db.connect("");
+				db.createDb(args[0]);
+				
+				// Run Bugzilla importer
+				
+			}
+		}
+		catch (ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Usage scm2pgsql <DB Name>");
+		}
 	}
 }
