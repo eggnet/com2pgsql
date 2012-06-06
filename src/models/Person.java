f1771df0aba4f1de3f966c4c49211cc8461fa556
Jordan Ell <jordan.ell7@gmail.com>
2012-06-06 09:17:11 -0700

    Added all the model classes for the project. fixes #1

--- src/models/Person.java
+++ src/models/Person.java
@@ -2,5 +2,50 @@ package models;
 
 public class Person
 {
+	private int 	PID;
+	private String 	name;
+	private String	email;
+	
+	public Person()
+	{
+		super();
+	}
 
+	public Person(int pID, String name, String email)
+	{
+		super();
+		PID = pID;
+		this.name = name;
+		this.email = email;
+	}
+
+	public int getPID()
+	{
+		return PID;
+	}
+
+	public void setPID(int pID)
+	{
+		PID = pID;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
 }
