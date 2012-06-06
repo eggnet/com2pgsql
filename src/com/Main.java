e183082eebd9fa95e72157b3372ad5caf7d88255
Jordan Ell <jordan.ell7@gmail.com>
2012-06-06 11:28:09 -0700

    Added the man page on incorrect use. fixes #6

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 import db.ComDb;
@@ -9,7 +11,7 @@ public class Main
 	public static ComDb db;
 	public static void main(String[] args)
 	{
-		System.out.println("Scm2Pgsql tool developed by eggnet at UVic.");
+		System.out.println("Com2Pgsql tool developed by eggnet at UVic.");
 		try {
 			if (args.length < 1)
 			{
@@ -18,17 +20,44 @@ public class Main
 			else
 			{
 				// Create the DB
-				db = new ComDb();
-				db.connect("");
-				db.createDb(args[0]);
+				//db = new ComDb();
+				//db.connect("");
+				//db.createDb(args[args.length-1]);
 				
 				// Run Bugzilla importer
+				String[] bugzillaArgs = bugzillaOptionCheck(args);
+				if(bugzillaArgs != null) {
+					
+				}
+				
 				
 			}
 		}
 		catch (ArrayIndexOutOfBoundsException e)
 		{
-			System.out.println("Usage scm2pgsql <DB Name>");
+			try {
+				// Print the man page
+				BufferedReader in = new BufferedReader(new FileReader("src/com/man.txt")); 
+				String line;
+				while ((line = in.readLine()) != null) {
+					System.out.println(line);          
+				}         
+				in.close();
+			}
+			catch (Exception ex) {
+				ex.printStackTrace();
+				System.out.println("There was an error printing the man page");
+			}
 		}
+	}
+	
+	private static String[] bugzillaOptionCheck(String args[]) {
+		for(int i = 0; i < args.length; i++) {
+			if(args[i].equals("-B")) {
+				String[] bugzillaArgs = {args[i+1], args[i+2]};
+				return bugzillaArgs;
+			}
+		}
+		return null;
 	}
 }
