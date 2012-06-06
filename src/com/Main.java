8ecb29c44fa73afa5f0b56982d5aff0d2239c69f
Jordan Ell <jordan.ell7@gmail.com>
2012-06-06 13:30:41 -0700

    This allows the use of run flag. fixes #8

--- src/com/Main.java
+++ src/com/Main.java
@@ -4,6 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+
 import db.ComDb;
 
 public class Main
@@ -12,6 +19,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("Com2Pgsql tool developed by eggnet at UVic.");
+		CommandLineParser parser = new GnuParser();
 		try {
 			if (args.length < 1)
 			{
@@ -19,45 +27,90 @@ public class Main
 			}
 			else
 			{
-				// Create the DB
-				//db = new ComDb();
-				//db.connect("");
-				//db.createDb(args[args.length-1]);
+				Options options = new Options();
 				
-				// Run Bugzilla importer
-				String[] bugzillaArgs = bugzillaOptionCheck(args);
-				if(bugzillaArgs != null) {
-					
+				Option bugzilla = OptionBuilder.withArgName("b")
+											   .hasArg()
+											   .create("b");
+				
+				Option database = OptionBuilder.withArgName("d")
+						   					   .hasArg()
+						   					   .create("d");
+				
+				Option email 	= OptionBuilder.withArgName("e")
+											   .hasArg()
+											   .create("e");
+				
+				options.addOption(bugzilla);
+				options.addOption(database);
+				options.addOption(email);
+				
+				CommandLine line = parser.parse(options, args);
+				
+				// Check for database
+				if(line.hasOption("d")) {
+				    String[] values = line.getOptionValues("d");
+				    if(values.length != 1) {
+				    	System.out.println("d flag used incorrectly.");
+				    	printMan();
+				    	return;
+				    }
+				    else {
+				    	// Create the DB
+				    	System.out.println("Creating database");
+						db = new ComDb();
+						db.connect("");
+						db.createDb(line.getOptionValue("d"));
+				    }
 				}
 				
+				// Check for bugzilla
+				if(line.hasOption("b")) {
+				    String[] values = line.getOptionValues("b");
+				    if(values.length != 1) {
+				    	System.out.println("b flag used incorrectly.");
+				    	printMan();
+				    	return;
+				    }
+				    else {
+				    	
+				    }
+				}
+				
+				// Check for email
+				if(line.hasOption("e")) {
+				    String[] values = line.getOptionValues("e");
+				    if(values.length != 1) {
+				    	System.out.println("e flag used incorrectly.");
+				    	printMan();
+				    	return;
+				    }
+				    else {
+				    	
+				    }
+				}
 				
 			}
 		}
-		catch (ArrayIndexOutOfBoundsException e)
+		catch (Exception e)
 		{
-			try {
-				// Print the man page
-				BufferedReader in = new BufferedReader(new FileReader("src/com/man.txt")); 
-				String line;
-				while ((line = in.readLine()) != null) {
-					System.out.println(line);          
-				}         
-				in.close();
-			}
-			catch (Exception ex) {
-				ex.printStackTrace();
-				System.out.println("There was an error printing the man page");
-			}
+			printMan();
 		}
 	}
 	
-	private static String[] bugzillaOptionCheck(String args[]) {
-		for(int i = 0; i < args.length; i++) {
-			if(args[i].equals("-B")) {
-				String[] bugzillaArgs = {args[i+1], args[i+2]};
-				return bugzillaArgs;
-			}
+	private static void printMan() {
+		try {
+			// Print the man page
+			BufferedReader in = new BufferedReader(new FileReader("src/com/man.txt")); 
+			String line;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);          
+			}         
+			in.close();
 		}
-		return null;
+		catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("There was an error printing the man page");
+		}
 	}
 }
