1860da3ed0544fdfe376c55c9697b7e54ecd3064
Jordan Ell <jordan.ell7@gmail.com>
2012-06-15 10:55:34 -0700

    Began writing linker for files mentioned. This may need to be changed a lot depending on how we want to do confidence level algorithm or what parameters we are willing to pass it. #23

--- src/db/LinkerDb.java
+++ src/db/LinkerDb.java
@@ -2,8 +2,13 @@ package db;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import comm.ComResources;
 
 import models.Commit;
 
@@ -37,5 +42,52 @@ public class LinkerDb extends DbConnection
 			return null;
 		}
 		return commits;
+	}
+	
+	public List<Commit> getCommitsAroundDate(Timestamp date) {
+		try {
+			List<Commit> commits = new ArrayList<Commit>();
+			
+			String sql = "SELECT commit_id, author, author_email, comments, commit_date, branch_id from commits WHERE" +
+					" (branch_id is NULL OR branch_id=?) AND" +
+					" commit_date <= ? + interval " + ComResources.COMMIT_DATE_MAX_RANGE + " day";
+			String[] parms = {branchID};
+			ResultSet rs = execPreparedQuery(sql, parms);
+			while(rs.next())
+			{
+				commits.add(new Commit(
+						rs.getString("commit_id"),
+						rs.getString("author"),
+						rs.getString("author_email"),
+						rs.getString("comments"),
+						rs.getTimestamp("commit_date"),
+						rs.getString("branch_id")
+				));
+			}
+			return commits;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public List<String> getFilesChangedOnCommit(Commit commit) {
+		try {
+			List<String> files = new ArrayList<String>();
+			String sql = "SELECT distinct file_id FROM file_diffs WHERE" +
+					" new_commit_id=?";
+			String[] parms = {commit.getCommit_id()};
+			ResultSet rs = execPreparedQuery(sql, parms);
+			while(rs.next())
+			{
+				files.add(rs.getString("file_id"));
+			}
+			return files;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
---
+++ src/email/linker/EmailLinker.java
@@ -0,0 +1,71 @@
+package email.linker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import db.ComDb;
+import db.LinkerDb;
+import linker.Linker;
+import models.Commit;
+import models.Item;
+import models.Person;
+
+public class EmailLinker extends Linker
+{
+	public EmailLinker(ComDb comDb, LinkerDb linkerDb)
+	{
+		super(comDb, linkerDb);
+	}
+	
+	@Override
+	public void Link()
+	{
+				
+	}
+	
+	public void linkItemsByMentionedFiles(Item item, List<Person> people, List<String> files) {
+		List<Commit> commits = linkerDb.getCommitsAroundDate(item.getItemDate());
+		List<models.Link> links = new ArrayList<models.Link>();
+		
+		for(Commit commit: commits) {
+			for(Person person: people) {
+				if(matchCommitToPerson(commit, person)) {
+					models.Link link = new models.Link(item.getItemId(), commit.getCommit_id(), 0.0f);
+					
+					// Get confidence up if possible
+					List<String> filesChanged = linkerDb.getFilesChangedOnCommit(commit);
+					link.setConfidence(fileMatchConfidence(files, filesChanged));
+					
+					// Insert
+					if(link.getConfidence() > 0)
+						links.add(link);
+				}
+			}
+		}
+	}
+	
+	private boolean matchCommitToPerson(Commit commit, Person person) {
+		return (person.getEmail().equals(commit.getAuthor_email()) ||
+				person.getName().equals(commit.getAuthor()));
+	}
+	
+	/**
+	 * This is the main confidence calculation for email items to 
+	 * commits in the social network.
+	 * @param files
+	 * @param filesChanged
+	 * @return
+	 */
+	private float fileMatchConfidence(List<String> files, List<String> filesChanged) {
+		float confidence = 0.0f;
+		
+		for(String file: files) {
+			if(filesChanged.contains(file))
+				confidence++;
+		}
+		
+		confidence /= files.size();
+		
+		return confidence;
+	}
+}
